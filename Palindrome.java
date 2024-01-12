72d651321b66701ea258f0cc7231a54a4b26b5b7
Sai <ksk@gmail.com>
2024-01-12 00:46:23 -0500

    Palindrome Program Assignment

---
+++ Palindrome.java
@@ -0,0 +1,19 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        String text ="abba";        //reading the given string
+        int len=text.length();      // finding the length of teh given string
+        boolean x=true;
+        for (int i = 0; i <len/2 ; i++) {
+            if (text.charAt(i) != text.charAt(len - i - 1)) {           //checking that the string is palindrome or not
+                x=false;
+                break;
+            }
+        }
+        if (x){
+            System.out.println("This is a palindrome");
+        }
+        else {
+            System.out.println("This is not a palindrome");
+        }
+    }
+}
