7c189a725e1a08ce9cb3817cf8a4d390d64e3760
Sai <ksk@gmail.com>
2024-01-14 11:24:25 -0500

    Palindrome program updated with StringBuilders

--- Palindrome.java
+++ Palindrome.java
@@ -1,19 +1,31 @@
 public class Palindrome {
     public static void main(String[] args) {
-        String text ="abba";        //reading the given string
-        int len=text.length();      // finding the length of teh given string
-        boolean x=true;
-        for (int i = 0; i <len/2 ; i++) {
+        String text = "abba";        //reading the given string
+        int len = text.length();      // finding the length of teh given string
+        boolean x = true;
+        for (int i = 0; i < len / 2; i++) {
             if (text.charAt(i) != text.charAt(len - i - 1)) {           //checking that the string is palindrome or not
-                x=false;
+                x = false;
                 break;
             }
         }
-        if (x){
+        if (x) {
             System.out.println("This is a palindrome");
-        }
-        else {
+        } else {
             System.out.println("This is not a palindrome");
         }
+        checkPalindrome("abba");
+    }
+    public static void checkPalindrome(String palindrometest){
+        StringBuilder check = new StringBuilder(palindrometest);
+        StringBuilder reversedString = check.reverse();
+        if (palindrometest.equals(reversedString.toString())){
+            System.out.println("It is palindrome");
+        }
+        else {
+            System.out.println("It is not a palindorme");
+        }
+
+
     }
 }
